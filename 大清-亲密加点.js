6eee41966743a9429c93b12d17f3bb815b2c3430
wutchsh <wutchsh@gmail.com>
2019-04-23 00:06:18 +0800

    new：急速亲密加点

---
+++ 大清-亲密加点.js
@@ -0,0 +1,25 @@
+//亲密加点
+auto();
+images.requestScreenCapture();sleep(100);
+var p1 = [875,1190];
+var p2 = [380,1190];
+function tappoint(p,n){
+    for(i=0;i<n;i++){
+        press(p[0],p[1],5);sleep(15);
+        if(i%20000 == 0){
+            log(i);
+            if(images.detectsColor(captureScreen(), "#ff7f7f7f", p[0],p[1])){
+                for(p=0;p<20;p++){
+                    device.vibrate(1000);sleep(500);
+                    device.cancelVibration();sleep(200);
+                }
+                exit();
+            }
+        }
+    }
+}
+tappoint(p1,500000);
+for(p=0;p<20;p++){
+    device.vibrate(1000);sleep(500);
+    device.cancelVibration();sleep(200);
+}
