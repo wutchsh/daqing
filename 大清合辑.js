a247e727acf67def96a66fb0c4f030df62e2bd40
wutchsh <wutchsh@gmail.com>
2019-04-26 18:26:56 +0800

    update:划船设定70s自动结束

--- 大清合辑.js
+++ 大清合辑.js
@@ -115,13 +115,6 @@ function _tuitu(){
 }
 
 function _huachuan(){
-    function tap(x, y, n) {
-        for (i = 0; i < 50000; i++) {
-            press(x, y, n);
-            sleep(n);
-        }
-    }
-    
     function main() {
         press(785,1580,10);sleep(300);
         press(755,1115,10);sleep(300);
@@ -138,6 +131,11 @@ function _huachuan(){
         threads.start(function() {
             tap(150, 1750, 10);
         });
+        threads.start(function() {
+            sleep(70000);
+            threads.shutDownAll();
+        });
+        sleep(70000);toastLog("划船结束！");
     }
     
     main();
