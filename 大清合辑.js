97c58ec14bb97eb7817c0b6e48181c24ceffd8e7
wutchsh <wutchsh@gmail.com>
2019-05-02 00:01:39 +0800

    new function:批量联姻

--- 大清合辑.js
+++ 大清合辑.js
@@ -179,3 +179,28 @@ function _ShengJi(){
     }
     upgrade();
 }
+
+function _JiangShiwang(){
+    // 僵尸王
+    while(images.detectsColor(captureScreen(), "#ff5d5d5d", 764,1480)){
+        press(1020,60,10);sleep(100);
+        press(880,945,10);sleep(200);
+    }
+    press(764,1480,10);sleep(50);
+    press(764,1480,10);sleep(100);
+    while(true){
+        press(800,1440,10);sleep(10);
+    }
+}
+
+function _LianYin(){
+    // 批量循环联姻
+    while(true){
+        press(880,525,10);sleep(500);
+        press(340,780,10);sleep(200);
+        press(340,1250,10);sleep(100);
+        for(i=0;i<5;i++){
+            press(540,800,10);sleep(100);
+        }
+    }
+}
