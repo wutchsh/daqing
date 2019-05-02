ccefcdf3d0ba378159d0e2ba7247a871aad038ed
wutchsh <wutchsh@gmail.com>
2019-05-02 21:10:35 +0800

    fix:修复无截图权限的报错

--- 大清-抢僵尸王.js
+++ 大清-抢僵尸王.js
@@ -1,4 +1,6 @@
 // 僵尸王
+auto();
+requestScreenCapture();sleep(100);
 while(images.detectsColor(captureScreen(), "#ff5d5d5d", 764,1480)){
     press(1020,60,10);sleep(100);
     press(880,945,10);sleep(200);
