58cc593e3fcd243183ba7c72d9e0bd1bc4772723
wutchsh <wutchsh@gmail.com>
2019-05-04 21:53:24 +0800

    update:增大进入活动界面到开始击杀的时间间隔，给时间其他人垫刀

--- 大清-抢僵尸王.js
+++ 大清-抢僵尸王.js
@@ -6,7 +6,7 @@ while(images.detectsColor(captureScreen(), "#ff5d5d5d", 764,1480)){
     press(880,945,10);sleep(200);
 }
 press(764,1480,10);sleep(50);
-press(764,1480,10);sleep(100);
+press(764,1480,10);sleep(400);  // 原始100ms，增大间隔，让其他人垫刀，延迟击杀
 while(true){
     press(800,1440,10);sleep(10);
 }
